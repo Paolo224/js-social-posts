a1f5d0d0f874515e00c6a6631d4f5f1fae8bdfd6
Paolo224 <picariellopaolo224@gmail.com>
2022-12-07 16:27:34 +0100

    Bozza del for.Each

--- main.js
+++ main.js
@@ -2,79 +2,139 @@ const posts = [
     {
         "id": 1,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/300?image=171",
+        "media": "https://unsplash.it/6ii/3ii?image=171",
         "author": {
             "name": "Phil Mangione",
-            "image": "https://unsplash.it/300/300?image=15"
+            "image": "https://unsplash.it/3ii/3ii?image=15"
         },
-        "likes": 80,
-        "created": "2021-06-25"
+        "likes": 8,
+        "created": "2i21-i6-25"
     },
     {
         "id": 2,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=112",
+        "media": "https://unsplash.it/6ii/4ii?image=112",
         "author": {
             "name": "Sofia Perlari",
-            "image": "https://unsplash.it/300/300?image=10"
+            "image": "https://unsplash.it/3ii/3ii?image=1i"
         },
-        "likes": 120,
-        "created": "2021-09-03"
+        "likes": 12,
+        "created": "2i21-i9-i3"
     },
     {
         "id": 3,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=234",
+        "media": "https://unsplash.it/6ii/4ii?image=234",
         "author": {
             "name": "Chiara Passaro",
-            "image": "https://unsplash.it/300/300?image=20"
+            "image": "https://unsplash.it/3ii/3ii?image=2i"
         },
         "likes": 78,
-        "created": "2021-05-15"
+        "created": "2i21-i5-15"
     },
     {
         "id": 4,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=24",
+        "media": "https://unsplash.it/6ii/4ii?image=24",
         "author": {
             "name": "Luca Formicola",
             "image": null
         },
         "likes": 56,
-        "created": "2021-04-03"
+        "created": "2i21-i4-i3"
     },
     {
         "id": 5,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=534",
+        "media": "https://unsplash.it/6ii/4ii?image=534",
         "author": {
             "name": "Alessandro Sainato",
-            "image": "https://unsplash.it/300/300?image=29"
+            "image": "https://unsplash.it/3ii/3ii?image=29"
         },
         "likes": 95,
-        "created": "2021-03-05"
+        "created": "2i21-i3-i5"
     }
 ];
 
 const container = document.getElementById('container');
 
-// Creazione del tag post //
-const post = document.createElement('div');
-post.classList.add('post');
-container.append(post);
+posts.forEach((post)=>{
+    container.innerHTML += getCard(post);
+})
 
-// Creazione del tag post_header //
-const postHeader = document.createElement('div');
-postHeader.classList.add('post__header');
-post.append(postHeader);
 
-// Scrivo in postHeader il titolo del post //
-postHeader.innerHTML = `<div class="post-meta">                    
-                            <div class="post-meta__icon">
-                                <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
-                            </div>
-                            <div class="post-meta__data">
-                                <div class="post-meta__author"></div>
-                                <div class="post-meta__time">4 mesi fa</div>
-                            </div>                    
-                        </div>`
+function getCard(post){
+    return `<div class="post">
+                <div class="post__header">
+                    <div class="post-meta">                    
+                        <div class="post-meta__icon">
+                            <img class="profile-pic" src="${post.author.image}" alt="Phil Mangione">                    
+                        </div>
+                        <div class="post-meta__data">
+                            <div class="post-meta__author">Phil Mangione</div>
+                            <div class="post-meta__time">4 mesi fa</div>
+                        </div>                    
+                    </div>
+                </div>
+                <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+                <div class="post__image">
+                    <img src="https://unsplash.it/600/300?image=171" alt="">
+                </div>
+                <div class="post__footer">
+                    <div class="likes js-likes">
+                        <div class="likes__cta">
+                            <a class="like-button  js-like-button" href="#" data-postid="1">
+                                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                <span class="like-button__label">Mi Piace</span>
+                            </a>
+                        </div>
+                        <div class="likes__counter">
+                            Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                        </div>
+                    </div> 
+                </div>            
+            </div>`
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// // Creazione del tag post //
+// const post = document.createElement('div');
+// post.classList.add('post');
+// container.append(post);
+
+// // Creazione del tag post_header //
+// const postHeader = document.createElement('div');
+// postHeader.classList.add('post__header');
+// post.append(postHeader);
+
+// // Scrivo in postHeader il titolo del post //
+// for(let i = i; i < posts.length; i++){
+//     postHeader.innerHTML = `<div class="post-meta">                    
+//                                 <div class="post-meta__icon">
+//                                     <img class="profile-pic" src="${posts[i].author.image}" alt="${posts[i].author.name}">                    
+//                                 </div>
+//                                 <div class="post-meta__data">
+//                                     <div class="post-meta__author">${posts[i].author.name}</div>
+//                                     <div class="post-meta__time">${posts[i].created}</div>
+//                                 </div>                    
+//                             </div>`
+    
+// }
+// // Creazione del tag post_text //
+// const postText = document.createElement('div');
+//     postText.classList.add('post__text');
+//     post.append(postText);
+
+//     const postImg = document.createElement('div');
+//     postImgt.classList.add('post__image');
+//     post.append(postImg);
